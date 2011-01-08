47981e6162c88f065f74fd63a409eb5996aad09f
Lauri <lauri.sakari.makinen@gmail.com>
2011-01-08 13:43:07 +0200

    Flipped the log history at the world view

--- api/engine/worldview.php
+++ api/engine/worldview.php
@@ -71,7 +71,7 @@ $result = query("SELECT * FROM feed WHERE target = '<$username>' OR target = '[g
 $count  = mysql_num_rows($result);
 
 
-for($i = 0;$i < $count;$i++){
+for($i = $count-1;$i > 0 ;$i--){
     $owner   = mysql_result($result,$i,"owner");
     $message = mysql_result($result,$i,"message");
     echo("<p><b>$owner</b>: $message</p>");
