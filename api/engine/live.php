ba4103137f02e86665f88674205570b5110041a6
Lauri <lauri.sakari.makinen@gmail.com>
2011-01-08 10:58:13 +0200

    Worldview now displays the world and tiles

--- api/engine/live.php
+++ api/engine/live.php
@@ -2,6 +2,15 @@
 $result = query("SELECT * FROM feed WHERE target = '<$username>' OR target = '[global]'");
 $count  = mysql_num_rows($result);
 
+$limit = $count - 10;
+if($limit < 0){
+    $limit = 0;
+}
+
+$result = query("SELECT * FROM feed WHERE target = '<$username>' OR target = '[global]' LIMIT $limit,10");
+$count  = mysql_num_rows($result);
+
+
 for($i = 0;$i < $count;$i++){
     $owner   = mysql_result($result,$i,"owner");
     $message = mysql_result($result,$i,"message");
--- api/engine/worldview.php
+++ api/engine/worldview.php
@@ -1,4 +1,8 @@
 <?php
+//TODO: move to data table
+$world_width = 10;
+$world_height = 10;
+
 $result = query("SELECT * FROM users WHERE username = '$username'");
 $count  = mysql_num_rows($result);
 
@@ -10,5 +14,55 @@ if($count == 1){
     die("Error on user profile!");
 }
 
-echo("$user_x $user_y $user_world");
+$result_world = query("SELECT * FROM world WHERE world = '$user_world'");
+$count_world  = mysql_num_rows($result_world);
+
+echo("<table border='1'>");
+for($y = 0;$y < $world_width;$y++){
+    echo("<tr>");
+    for($x = 0;$x < $world_height;$x++){
+        echo("<td>");
+
+            //get tile
+            $hit = false;
+            for($i = 0;$i < $count_world;$i++){
+                if($x == mysql_result($result_world,$i,"posx") AND $y == mysql_result($result_world,$i,"posy")){
+                    $tile = mysql_result($result_world,$i,"tile");
+                    echo("<b>$tile</b>");
+                    $hit = true;
+                }
+            }
+
+            if($hit == false){
+                echo("null"); 
+            }
+
+            if($x == $user_x AND $y == $user_y){
+                echo("O");
+            }
+        echo("</td>");
+    }
+    echo("</tr>");
+}
+echo("</table>");
+echo("<hr>");
+
+$result = query("SELECT * FROM feed WHERE target = '<$username>' OR target = '[global]'");
+$count  = mysql_num_rows($result);
+
+$limit = $count - 10;
+if($limit < 0){
+    $limit = 0;
+}
+
+$result = query("SELECT * FROM feed WHERE target = '<$username>' OR target = '[global]' LIMIT $limit,10");
+$count  = mysql_num_rows($result);
+
+
+for($i = 0;$i < $count;$i++){
+    $owner   = mysql_result($result,$i,"owner");
+    $message = mysql_result($result,$i,"message");
+    echo("<p><b>$owner</b>: $message</p>");
+}
+
 ?>
