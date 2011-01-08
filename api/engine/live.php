61d5938dbee7ce78b0cccc79f78df3a91c93d1a1
Lauri <lauri.sakari.makinen@gmail.com>
2011-01-08 02:36:54 +0200

    Display live feed from chat

--- api/engine/live.php
+++ api/engine/live.php
@@ -1,3 +1,10 @@
 <?php
-echo rand(1,100);
+$result = query("SELECT * FROM feed");
+$count  = mysql_num_rows($result);
+
+for($i = 0;$i < $count;$i++){
+    $owner   = mysql_result($result,$i,"owner");
+    $message = mysql_result($result,$i,"message");
+    echo("<p><b>$owner</b>: $message</p>");
+}
 ?>
